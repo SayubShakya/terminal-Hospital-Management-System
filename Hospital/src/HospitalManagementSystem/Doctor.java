840b002d05fcea8e264a5892cea944417abfa3c6
Sayub <shakya.sayub123@gmail.com>
2025-02-25 21:09:03 +0545

    Push System

---
+++ Hospital/src/HospitalManagementSystem/Doctor.java
@@ -0,0 +1,52 @@
+package HospitalManagementSystem;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class Doctor {
+    private Connection connection;
+
+    public Doctor(Connection connection) {
+        this.connection = connection;
+    }
+
+    public void viewDoctors() {
+        String query = "SELECT * FROM doctors";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query);
+                ResultSet resultSet = preparedStatement.executeQuery()) {
+
+            System.out.println("Doctors: ");
+            System.out.println("| ID | Name             | Specialization |");
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String specialization = resultSet.getString("specialization");
+
+                System.out.println(id +" " +name+ " "+specialization);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean getDoctorById(int id) {
+        String query = "SELECT * FROM doctors WHERE id = ?";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
